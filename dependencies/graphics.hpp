2eb1e8058672f703b203ff4255b692ce00d17bc2
krush11 <krushnalpatel11@gmail.com>
2020-11-04 14:21:34 +0530

    added graphics.hpp

---
+++ dependencies/graphics.hpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+using namespace std;
+
+void gotoxy(short int x, short int y)
+{
+    static HANDLE h = NULL;
+    if (!h)
+        h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD c = {x, y};
+    SetConsoleCursorPosition(h, c);
+}
