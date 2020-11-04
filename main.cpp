b4986bedcde1e66228a7aad43c078f552116019a
krush11 <krushnalpatel11@gmail.com>
2020-11-04 14:20:05 +0530

    updated encode

--- main.cpp
+++ main.cpp
@@ -2,33 +2,39 @@
 #include <iostream>
 #include <unordered_map>
 #include "dependencies/conversions.hpp"
-#include "dependencies/tree.hpp"
 #include "dependencies/map.hpp"
+#include "dependencies/tree.hpp"
 using namespace std;
 
-unordered_map<char, int> freq,temp;
-int totcount = 0;   
+unordered_map<char, int> freq, temp;
+int totcount = 0;
 unordered_map<char, string> freq2;
 
 char find_key_value(int data)
 {
-    for(auto i : temp)
+    for (auto i : temp)
     {
-        if(i.second == data)
+        if (i.second == data)
             return i.first;
     }
 }
 
 void encode(hufftree *root, string s = '\0')
 {
-    if(root == NULL)
+    if (root->left == NULL && root->right == NULL)
     {
-        freq2[find_key_value(root ->data)] = s;
+        freq2[find_key_value(root->data)] = s;
+        return;
     }
-
-    encode(root ->right, s+'1');
-    encode(root ->left, s+'0');
-
+    else if (root->left == NULL)
+        encode(root->right, s + '1');
+    else if (root->right == NULL)
+        encode(root->left, s + '0');
+    else
+    {
+        encode(root->right, s + '1');
+        encode(root->left, s + '0');
+    }
 }
 
 hufftree *implement_tree()
@@ -36,7 +42,7 @@ hufftree *implement_tree()
     hufftree *root = new hufftree(totcount);
     hufftree *curr = root;
     int temp_count = freq.size();
-    for(int i=0; i<temp_count; i++)
+    for (int i = 0; i < temp_count; i++)
     {
         char ch = max_freq(freq);
         int value = freq[ch];
@@ -44,10 +50,10 @@ hufftree *implement_tree()
         temp[ch] = value;
 
         hufftree *node = new hufftree(temp[ch]);
-        curr ->right = node;
-        node = new hufftree(curr ->data - temp[ch]);
-        curr ->left = node;
-        curr = curr ->left;
+        curr->right = node;
+        node = new hufftree(curr->data - temp[ch]);
+        curr->left = node;
+        curr = curr->left;
     }
     return root;
 }
